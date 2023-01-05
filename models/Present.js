8246c08b0ade6b63d245edad0faee502569291c1
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-01-05 18:32:28 +0100

    work in progress

--- models/Present.js
+++ models/Present.js
@@ -2,5 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const presentSchema = new Schema ({
-    
+    name: {
+        type: String,
+    },
+    image: String,
+    price: Number,
+    recipient: String,
+    description: {
+        type: String,
+    }
 })
+
+const Present = mongoose.model('Present', presentSchema);
+module.exports = Present;
